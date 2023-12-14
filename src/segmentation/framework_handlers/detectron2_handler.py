3ee5bc6dc572a0c62d1829bba56f219b893f2252
elorberb <etay.lorberboym@intel.com>
2023-12-14 17:50:24 +0200

    update the detectron2 working class and notebook

--- src/segmentation/framework_handlers/detectron2_handler.py
+++ src/segmentation/framework_handlers/detectron2_handler.py
@@ -16,11 +16,13 @@ from segments.utils import export_dataset
 from src.annotation_handling.segmentsai_handler import SegmentsAIHandler
 import os
 import numpy as np
-from datetime import datetime
-import yaml
 from matplotlib import pyplot as plt
 import cv2
 import torch
+import csv
+from skimage.measure import regionprops, label
+import seaborn as sns
+import pandas as pd
 
 # Global variables
 SEGMENTS_HANDLER = SegmentsAIHandler()
@@ -33,23 +35,21 @@ def print_version_info():
     print("detectron2:", detectron2.__version__)
 
 
-def convert_detectron2_to_segments_format(image, outputs):
+def convert_coco_to_segments_format(image, outputs):
     segmentation_bitmap = np.zeros((image.shape[0], image.shape[1]), np.uint32)
     annotations = []
     counter = 1
-    instances = outputs["instances"]
+    instances = outputs['instances']
     for i in range(len(instances.pred_classes)):
         category_id = int(instances.pred_classes[i])
         instance_id = counter
         mask = instances.pred_masks[i].cpu()
         segmentation_bitmap[mask] = instance_id
-        annotations.append(
-            {"id": instance_id, "category_id": category_id}
-        )
+        annotations.append({'id': instance_id, 'category_id': category_id})
         counter += 1
     return segmentation_bitmap, annotations
 
-def convert_segments_to_detectron2_format(dataset_name, release_version, export_format="coco-instance", output_dir="."):
+def convert_segments_to_coco_format(dataset_name, release_version, export_format="coco-instance", output_dir="."):
     # get the dataset instance
     dataset = SEGMENTS_HANDLER.get_dataset_instance(dataset_name, version=release_version)
         
@@ -58,212 +58,143 @@ def convert_segments_to_detectron2_format(dataset_name, release_version, export_
     return dataset, export_json_path, saved_images_path
 
 
-class Detectron2Handler:
+def imshow(image):
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)# Plot the image
+    plt.imshow(image_rgb)
+    plt.show()
+
+
+def prepare_and_register_datasets(dataset_name_train, dataset_name_test, release_train, release_test, saving_path):
+    # Convert segments dataset to coco format for training dataset
+    _, train_export_json_path, train_saved_images_path = convert_segments_to_coco_format(
+        dataset_name=dataset_name_train, 
+        release_version=release_train, 
+        output_dir=saving_path
+    )
+
+    # Convert segments dataset to coco format for testing dataset
+    _, test_export_json_path, test_saved_images_path = convert_segments_to_coco_format(
+        dataset_name=dataset_name_test, 
+        release_version=release_test, 
+        output_dir=saving_path
+    )
+
+    # Register the coco format datasets
+    register_coco_instances("my_dataset_train", {}, train_export_json_path, train_saved_images_path)
+    register_coco_instances("my_dataset_val", {}, test_export_json_path, test_saved_images_path)
+
+    # Get the metadata and dataset dicts
+    metadata_train = MetadataCatalog.get("my_dataset_train")
+    dataset_dicts_train = DatasetCatalog.get("my_dataset_train")
+    metadata_test = MetadataCatalog.get("my_dataset_val")
+    dataset_dicts_test = DatasetCatalog.get("my_dataset_val")
+
+    return metadata_train, dataset_dicts_train, metadata_test, dataset_dicts_test
+
+
+
+
+def plot_train_samples(dataset_dicts_train, metadata_train, indices=None, scale=0.5):
+    """
+    Plots samples based on specified indices.
+
+    Parameters:
+    - indices (list): List of specific indices of samples to plot.
+    - scale (float): Scale factor for the visualizer.
     
-    def __init__(self, **kwargs):
-        """
-        Initializes the Detectron2Handler with the given keyword arguments.
+    Example usage:
+    model - Detectron2Handler(...)
+    model.plot_samples(indices=[0, 2, 5]) # to plot images at specific indices
+    model..plot_samples() # to plot all images
+    """
+    selected_samples = dataset_dicts_train if indices is None else [dataset_dicts_train[i] for i in indices]
 
-        Keyword Arguments:
-        - config_path (str): Path to the Detectron2 configuration file.
-        - model_weights_url (str): URL or local path to the model weights.
-        - train_dataset_name (str): Name of the train dataset.
-        - train_release (str): Version of the train dataset.
-        - test_dataset_name (str): Name of the test dataset.
-        - test_release (str): Version of the test dataset.
-        - export_format (str, optional): Format for exporting the dataset, default is 'coco-instance'.
-        - input_mask_format (str, optional): Format of the input mask, default is 'bitmask'.
-        - model_device (str, optional): Device to run the model on, default is 'cuda'.
-        """
-        
-        setup_logger()
-
-        # Extracting configuration settings from kwargs
-        self.train_dataset_name = kwargs.get('train_dataset_name')
-        train_release = kwargs.get('train_release')
-        self.test_dataset_name = kwargs.get('test_dataset_name')
-        test_release = kwargs.get('test_release')
-        export_format = kwargs.get('export_format', 'coco-instance')
-        input_mask_format = kwargs.get('input_mask_format', 'bitmask')
-        model_device = kwargs.get('model_device', 'cuda')
-        
-        self.task_type = kwargs.get('task_type')
-        self.model_type = kwargs.get('model_type')
-        model_config_path = f"{self.task_type}/{self.model_type}.yaml"
-                
-        # Setting up the output directory
-        timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M")
-        output_dir = f"{DETECTRON2_CHECKPOINT_BASE_PATH}/{self.task_type}/{self.model_type}/{timestamp}"
-
-        # convert segments dataset to coco format
-        self.train_dataset, train_export_json_path, train_saved_images_path = convert_segments_to_detectron2_format(
-            dataset_name=self.train_dataset_name, 
-            release_version=train_release, 
-            export_format=export_format, 
-            output_dir=output_dir
-        )
-        
-        self.test_dataset, test_export_json_path, test_saved_images_path = convert_segments_to_detectron2_format(
-            dataset_name=self.test_dataset_name, 
-            release_version=test_release, 
-            export_format=export_format, 
-            output_dir=output_dir
-        )
-        # register the coco format datasets
-        register_coco_instances("my_dataset_train", {}, train_export_json_path, train_saved_images_path)
-        register_coco_instances("my_dataset_val", {}, test_export_json_path, test_saved_images_path)
-
-        # get the metadata and dataset dicts
-        self.train_metadata = MetadataCatalog.get("my_dataset_train")
-        self.train_dataset_dicts = DatasetCatalog.get("my_dataset_train")
-        self.test_metadata = MetadataCatalog.get("my_dataset_val")
-        self.test_dataset_dicts = DatasetCatalog.get("my_dataset_val")
+    for d in selected_samples:
+        img = cv2.imread(d["file_name"])
+        visualizer = Visualizer(img[:, :, ::-1], metadata=metadata_train, scale=scale)
+        out = visualizer.draw_dataset_dict(d)
+        image_rgb = cv2.cvtColor(out.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
+        plt.imshow(image_rgb)
+        plt.show()
 
 
-    # Current setup for training the model with default values (I should work also on testing different values)
-    def setup_training(
-        self,
-        score_thresh_test=0.5,
-        num_workers=2,
-        ims_per_batch=2,
-        base_lr=0.00025,
-        max_iter=1000,
-        batch_size_per_image=512,
-        num_classes=4,
-    ):
-        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = score_thresh_test
-        self.cfg.DATALOADER.NUM_WORKERS = num_workers
-        self.cfg.SOLVER.IMS_PER_BATCH = ims_per_batch
-        self.cfg.SOLVER.BASE_LR = base_lr
-        self.cfg.SOLVER.MAX_ITER = max_iter
-        self.cfg.SOLVER.STEPS = []        # do not decay learning rate
-        self.cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = batch_size_per_image
-        # If num_classes is not provided, calculate it from dataset.categories
-        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes # Currently 4 classes: trichome, clear, cloudy and amber - can use this if not working: len(self.dataset.categories) if num_classes is None else num_classes    
-        self.trainer = DefaultTrainer(self.cfg)
-        self.trainer.resume_or_load(resume=False)
-
-
-
-    def train(self):
-        # Training the model
-        self.trainer.train()
-        
-        # Saving the model
-        self.cfg.MODEL.WEIGHTS = os.path.join(self.cfg.OUTPUT_DIR, "model_final.pth")
-        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set a custom testing threshold
-        self.predictor = DefaultPredictor(self.cfg)
-        
-        # Saving the configuration
-        config_yaml_path = os.path.join(self.cfg.OUTPUT_DIR, "config.yaml")
-        with open(config_yaml_path, 'w') as f:
-            yaml.dump(self.cfg, f)
-            
+def plot_test_predictions(dataset_dicts_test, metadata_test, predictor, indices=None, scale=0.5):
     
-    def plot_train_samples(self, indices=None, scale=0.5):
-        """
-        Plots samples based on specified indices.
+    selected_samples = dataset_dicts_test if indices is None else [dataset_dicts_test[i] for i in indices]
 
-        Parameters:
-        - indices (list): List of specific indices of samples to plot.
-        - scale (float): Scale factor for the visualizer.
-        
-        Example usage:
-        model - Detectron2Handler(...)
-        model.plot_samples(indices=[0, 2, 5]) # to plot images at specific indices
-        model..plot_samples() # to plot all images
-        """
-        selected_samples = self.train_dataset_dicts if indices is None else [self.train_dataset_dicts[i] for i in indices]
-
-        for d in selected_samples:
-            img = cv2.imread(d["file_name"])
-            visualizer = Visualizer(img[:, :, ::-1], metadata=self.train_metadata, scale=scale)
-            out = visualizer.draw_dataset_dict(d)
-            image_rgb = cv2.cvtColor(out.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
-            plt.imshow(image_rgb)
-            plt.show()
-            
-
-    def plot_test_predictions(self, indices=None, scale=0.5):
-        
-        selected_samples = self.test_dataset_dicts if indices is None else [self.test_dataset_dicts[i] for i in indices]
-
-        for d in selected_samples:
-            im = cv2.imread(d["file_name"])
-            outputs = self.predictor(im)
-            v = Visualizer(im[:, :, ::-1], metadata=self.test_metadata, scale=scale, 
-                        instance_mode=ColorMode.IMAGE_BW)
-            out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-            image_rgb = cv2.cvtColor(out.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
-            plt.imshow(image_rgb)
-            plt.show()
+    for d in selected_samples:
+        im = cv2.imread(d["file_name"])
+        outputs = predictor(im)
+        v = Visualizer(im[:, :, ::-1], metadata=metadata_test, scale=scale, 
+                    instance_mode=ColorMode.IMAGE)
+        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        image_rgb = cv2.cvtColor(out.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
+        plt.imshow(image_rgb)
+        plt.show()
 
 
-    def evaluate(self):
-        """
-        Evaluate the model on the specified dataset.
-
-        Parameters:
-        dataset_name (str): The name of the dataset to evaluate on.
-        """
-        # Set the dataset for testing
-        evaluator = COCOEvaluator("my_dataset_val", output_dir=os.path.join(self.cfg.OUTPUT_DIR, "output"))
-        val_loader = build_detection_test_loader(self.cfg, "my_dataset_val")
-        print(inference_on_dataset(self.predictor.model, val_loader, evaluator))
-
-        
-    
-    def load_checkpoint(self, checkpoint_path, config_yaml_path=None):
-        """
-        Load a specific model weights checkpoint.
-
-        Parameters:
-        checkpoint_path (str): The file path to the checkpoint.
-        """
-        # Update the model configuration to use the specified checkpoint
-        self.cfg.MODEL.WEIGHTS = checkpoint_path
-        
-        # Load the configuration from the specified yaml file
-        if config_yaml_path is not None:
-            self.cfg.merge_from_file(config_yaml_path)
-
-        # Re-initialize the model predictor with the updated configuration
-        self.predictor = DefaultPredictor(self.cfg)
+def evaluate_model_on_dataset(cfg, predictor):
+    evaluator = COCOEvaluator("my_dataset_val", output_dir=os.path.join(cfg.OUTPUT_DIR, "eval_output"))
+    val_loader = build_detection_test_loader(cfg, "my_dataset_val")
+    return inference_on_dataset(predictor.model, val_loader, evaluator)
 
 
+def extract_object_info_to_csv(input_images_directory, output_csv_path, predictor, metadata):
+    with open(output_csv_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["File Name", "Class Name", "Object Number", "Area", "Centroid", "BoundingBox"])
 
-    def predict_image(self, image_path, plot_image=False):
-        image = cv2.imread(image_path)
-        outputs = self.predictor(image)
-        if plot_image:
-            # We can use `Visualizer` to draw the predictions on the image.
-            v = Visualizer(image[:, :, ::-1], metadata=self.train_metadata)
-            out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-            image_rgb = cv2.cvtColor(out.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)# Plot the image
-            plt.imshow(image_rgb)
-            plt.show()
-        segmentation_bitmap, annotations = convert_detectron2_to_segments_format(
-            image, outputs
-        )
-        return segmentation_bitmap, annotations
-    
-    # TODO: this code is not working well
-    # TODO: need to fix it and find a different may to reuse code for detectron2 notebooks
+        for image_filename in os.listdir(input_images_directory):
+            image_path = os.path.join(input_images_directory, image_filename)
+            new_im = cv2.imread(image_path)
+            outputs = predictor(new_im)
+            mask = outputs["instances"].pred_masks.to("cpu").numpy().astype(bool)
+            class_labels = outputs["instances"].pred_classes.to("cpu").numpy()
+            labeled_mask = label(mask)
+            props = regionprops(labeled_mask)
+
+            for i, prop in enumerate(props):
+                object_number = i + 1
+                area = prop.area
+                centroid = prop.centroid
+                bounding_box = prop.bbox
+                class_label = class_labels[i] if i < len(class_labels) else 'Unknown'
+                class_name = metadata.thing_classes[class_label] if class_label != 'Unknown' else 'Unknown'
+                csvwriter.writerow([image_filename, class_name, object_number, area, centroid, bounding_box])
+
+    return f"Object-level information saved to CSV file at {output_csv_path}"
 
 
-# if __name__ == "__main__":
+def plot_class_statistics(output_csv_path, metadata_train):
+    # Load the CSV file into a pandas DataFrame
+    df = pd.read_csv(output_csv_path)
 
-#     model_config = {
-#     'task_type': 'COCO-InstanceSegmentation',
-#     'model_type': 'mask_rcnn_R_50_FPN_3x',
-#     'dataset_name': 'etaylor/cannabis_patches_all_images',
-#     'release_version': "v0.2",
-#     'export_format': "coco-instance",
-#     'model_device': "cuda",
-#     }
+    # Get class names from train_metadata.thing_classes
+    class_names = metadata_train.thing_classes
 
-#     handler = Detectron2Handler(**model_config)
+    # Calculate the average number of objects per image for each class
+    avg_objects_per_class = df.groupby(["File Name", "Class Name"])["Object Number"].count().reset_index()
+    avg_objects_per_class = avg_objects_per_class.groupby("Class Name")["Object Number"].mean().reset_index()
 
-#     handler.setup_training()
-#     handler.train() 
- 
+    # Plot: Average number of objects per image for each class
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x="Class Name", y="Object Number", data=avg_objects_per_class, ci=None, order=class_names)
+    plt.xticks(rotation=45)
+    plt.xlabel("Class Name")
+    plt.ylabel("Average Number of Objects per Image")
+    plt.title("Average Number of Objects per Image for Each Class")
+    plt.tight_layout()
+    plt.show()
+
+    # Calculate the average area of objects for each class
+    avg_area_per_class = df.groupby("Class Name")["Area"].mean().reset_index()
+
+    # Plot: Average area of objects for each class
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x="Class Name", y="Area", data=avg_area_per_class, ci=None, order=class_names)
+    plt.xticks(rotation=45)
+    plt.xlabel("Class Name")
+    plt.ylabel("Average Area of Objects")
+    plt.title("Average Area of Objects for Each Class")
+    plt.tight_layout()
+    plt.show()
