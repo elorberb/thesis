fd467d66ebb106b4dc64bba7342b1e9b04fbd644
elorberb <etay.lorberboym@intel.com>
2024-02-09 22:40:10 +0200

    modify the db utils functions and creating a notebook to interact with the participants feedback

--- src/app/db_utils.py
+++ src/app/db_utils.py
@@ -21,18 +21,10 @@ def save_participant_registration(document):
 
 def save_experiment_feedback(document):
     db = connect_to_database()
-    participant_id = document.get('participant_id')
-    # Ensure there's a unique identifier for each feedback entry, e.g., feedback ID or using a timestamp
-    feedback_id = document.get('feedback_id')  # Assuming each feedback has a unique ID
-    doc_ref = db.collection(const.FIREBASE_EXPERIMENT_FEEDBACK_COLLECTION).document(participant_id).collection('Feedback').document(feedback_id)
-    doc_ref.set(document)
+    db.collection(const.FIREBASE_EXPERIMENT_FEEDBACK_COLLECTION).add(document)
     pp.pprint(f'Experiment feedback = {document} saved successfully to {const.FIREBASE_EXPERIMENT_FEEDBACK_COLLECTION} collection.')
 
 def save_post_questionnaire_feedback(document):
     db = connect_to_database()
-    participant_id = document.get('participant_id')
-    # Similar to feedback, assume there's a unique identifier for each questionnaire response
-    questionnaire_id = document.get('questionnaire_id')  # Assuming each questionnaire response has a unique ID
-    doc_ref = db.collection(const.FIREBASE_POST_QUESTIONNAIRE_FEEDBACK_COLLECTION).document(participant_id).collection('QuestionnaireFeedback').document(questionnaire_id)
-    doc_ref.set(document)
-    pp.pprint(f'Post-questionnaire feedback = {document} saved successfully to {const.FIREBASE_POST_QUESTIONNAIRE_FEEDBACK_COLLECTION} collection.')
+    db.collection(const.FIREBASE_POST_QUESTIONNAIRE_FEEDBACK_COLLECTION).add(document)
+    pp.pprint(f'Post-questionnaire feedback  = {document} saved successfully to {const.FIREBASE_POST_QUESTIONNAIRE_FEEDBACK_COLLECTION} collection.')
